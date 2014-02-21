afa84a74ca97fc9229b28d7d4d050b89b6b7c28d
Stephen Cummins <sacummins@gmail.com>
2014-02-21 16:25:05 +0000

    readded html conversion for concept pages as part of the latex convert workflow

--- src/main/python/segue/etl/latexConvert.py
+++ src/main/python/segue/etl/latexConvert.py
@@ -68,7 +68,7 @@ class LaTeXContentBuilder(ContentBuilderBase):
 
             self.convertToMarkupRecursively(contentObject.__dict__)
 
-            #contentObject.content = convertToHtml(self.jsonMetaData, self.latexSourceContents)
+            contentObject.content = convertToHtml(self.jsonMetaData, self.latexSourceContents)
             #contentObject.encoding = "html"
         elif 'legacy_latex_question' in self.jsonMetaData['type']: #or any type of question really
             # we want to extract the parts of the question that we are interested in e.g exposition, question, answers etc
